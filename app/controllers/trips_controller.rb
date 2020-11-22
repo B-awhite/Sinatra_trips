36821fc8156206431af50663fa4c59ed6330abba
bria_white <bria.awhite@gmail.com>
2020-11-22 18:24:21 -0500

    CRUD and trip views

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,9 +5,36 @@ class TripsController < ApplicationController
     erb :'trips/index'
   end 
 
+  get '/trips/new' do 
+    erb :'/trips/new'
+  end
+
   get '/trips/:id' do 
-    @trip = Trip.find_by(id:params[:id])
+    @trip = Trip.find_by_id(params[:id])
     erb :'trips/show'
   end 
+
+  post '/trips' do 
+    trip = Trip.create(params[:trip])
+    redirect "/trips/#{trip.id}"
+  end
+
+  get '/trips/:id/edit' do 
+    @trip = Trip.find_by(id:params[:id])
+    erb :"/trips/edit"
+  end
+
+  patch '/trips/:id' do 
+    trip = Trip.find_by(id:params[:id])
+    trip.update(params[:trip])
+    redirect "/trips/#{trip.id}"
+  end 
+
+  delete '/trips/:id' do 
+    trip = Trip.find_by(id:params[:id])
+    trip.destroy
+    redirect "/trips"
+  end
+  
 
 end 
