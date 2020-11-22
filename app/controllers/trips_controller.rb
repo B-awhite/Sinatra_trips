3f6d3feb29caeb338df63b27ee7464dc440ca928
bria_white <bria.awhite@gmail.com>
2020-11-22 15:29:56 -0500

    Seeds, Basic views, Read crud

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,3 +1,13 @@
 class TripsController < ApplicationController
 
+  get '/trips' do 
+    @trips = Trip.all
+    erb :'trips/index'
+  end 
+
+  get '/trips/:id' do 
+    @trip = Trip.find_by(id:params[:id])
+    erb :'trips/show'
+  end 
+
 end 
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+# Trip.create(name:"Italy", city:"Rome", image:"https://i.ytimg.com/vi/UDlBySgEJ38/maxresdefault.jpg")
+# Trip.create(name:"Greece", city:"Athens", image:"https://i.ytimg.com/vi/vAeW7WgJq54/maxresdefault.jpg")
+# Trip.create(name:"France", city:"Paris", image:"https://www.thespiritsbusiness.com/content/http://www.thespiritsbusiness.com/media/2020/07/Paris.jpg")
