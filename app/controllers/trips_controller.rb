2207291f5e018950cfcc81edfffa191c0962ad20
bria_white <bria.awhite@gmail.com>
2020-11-22 21:35:04 -0500

    Helper methods and signup

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,39 +7,49 @@ class TripsController < ApplicationController
   get '/trips' do 
     @user = current_user
      @trips = current_user.trips
-      erb :'trips/index'
+      erb :"/trips/index"
   end 
 
   get '/trips/new' do 
-    erb :'/trips/new'
+    erb :"/trips/new"
   end
 
-  get '/trips/:id' do 
-    @trip = Trip.find_by_id(params[:id])
-    erb :'trips/show'
-  end 
-
   post '/trips' do 
-    trip = Trip.create(params[:trip])
+    trip = current_user.trips.create(params[:trip])
     redirect "/trips/#{trip.id}"
   end
 
+  get '/trips/:id' do 
+    search_for_trip
+    if @trip
+      erb :"trips/show"
+    else
+      redirect "/trips"
+    end 
+  end 
+
   get '/trips/:id/edit' do 
-    @trip = Trip.find_by(id:params[:id])
+    search_for_trip
     erb :"/trips/edit"
   end
 
   patch '/trips/:id' do 
-    trip = Trip.find_by(id:params[:id])
-    trip.update(params[:trip])
+    search_for_trip
+    @trip.update(params[:trip])
     redirect "/trips/#{trip.id}"
   end 
 
   delete '/trips/:id' do 
-    trip = Trip.find_by(id:params[:id])
-    trip.destroy
+    search_for_trip
+    @trip.destroy
     redirect "/trips"
   end
+
+  private 
+  
+  def search_for_trip
+    @trip = Trip.find_by(id:params[:id])
+  end 
   
 
 end 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController 
 
+    get "/signup" do 
+        erb :"/users/signup"
+      end 
+      
+      post "/signup" do 
+         user = User.create(params)
+         if user.save
+            session[:user_id] = user.id
+            redirect "/trips"
+         else
+             @error = "Can't be blank"
+             erb :"/users/signup"
+         end
+      end
+
 end 
